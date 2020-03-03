8a1666c2394abd94ed2abaa38120e76de4b2bf4b
GoudekettingRM <r.goudeketting@gmail.com>
2020-03-03 14:58:11 +0100

    add players get all route including get by team id

--- player/router.js
+++ player/router.js
@@ -9,10 +9,48 @@ router.post("/players", async (req, res, next) => {
     const hashedPassword = bcrypt.hashSync(req.body.password, 10);
     const newPlayerData = { ...req.body, password: hashedPassword };
     const player = await Player.create(newPlayerData);
-    console.log("player test", player.dataValues);
 
     const { password, ...playerInfo } = player.dataValues;
+
     res.json(playerInfo);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/players", async (req, res, next) => {
+  try {
+    if (!Object.keys(req.body).length) {
+      const allPlayers = await Player.findAll();
+      if (!allPlayers.length) {
+        res
+          .status(404)
+          .send({ message: "No players found" })
+          .end();
+      }
+      const allPlayersWithoutPassword = allPlayers.map(player => {
+        const { password, ...playerWithoutPassword } = player.dataValues;
+        return playerWithoutPassword;
+      });
+
+      res.json(allPlayersWithoutPassword);
+    } else {
+      const teamPlayers = await Player.findAll({
+        where: { teamId: req.body.teamId }
+      });
+      if (!teamPlayers.length) {
+        res
+          .status(404)
+          .send({ message: "No players found" })
+          .end();
+      }
+      const teamPlayersWithoutPassword = teamPlayers.map(player => {
+        const { password, ...playerWithoutPassword } = player.dataValues;
+        return playerWithoutPassword;
+      });
+
+      res.json(teamPlayersWithoutPassword);
+    }
   } catch (err) {
     next(err);
   }
