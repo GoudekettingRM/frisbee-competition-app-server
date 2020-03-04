cab898855e2dbe613b6cd53a2578d1ad60e1e918
GoudekettingRM <r.goudeketting@gmail.com>
2020-03-04 16:18:01 +0100

    expand organisation post endpoint to check if user already is contact for another organisation and prohibit creation if so. Otherwise add new organisation id to user

--- organisation/router.js
+++ organisation/router.js
@@ -1,13 +1,31 @@
 const { Router } = require("express");
 const { auth } = require("../auth/authMiddleware");
 const Organisation = require("./model");
+const User = require("../user/model");
 
 const router = new Router();
 
-router.post("/organisations", async (req, res, next) => {
+router.post("/organisations", auth, async (req, res, next) => {
   try {
+    if (req.user.organisationId) {
+      res
+        .status(400)
+        .send({
+          message: `New organisation not created. User already is the contact for another organisation.`
+        })
+        .end();
+    }
     const newOrganisation = await Organisation.create(req.body);
-    res.json(newOrganisation);
+    const updatedUser = await User.update(
+      { organisationId: newOrganisation.id },
+      { where: { id: req.user.id } }
+    );
+    res.json({
+      message:
+        "New organisation created successfully. User has been registered as contact for organisation.",
+      newOrganisation,
+      updatedUser
+    });
   } catch (error) {
     next(error);
   }
