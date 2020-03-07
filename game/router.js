e77895b0f309895874053bf6a5777a691116cd45
GoudekettingRM <r.goudeketting@gmail.com>
2020-03-07 23:02:46 +0100

    optimise super admin create game

--- game/router.js
+++ game/router.js
@@ -9,56 +9,56 @@ const router = new Router();
 
 router.post("/games", auth, async (req, res, next) => {
   try {
-    console.log("Req.body games test", req.body);
-
-    if (!req.user.organisationId) {
-      console.log("Post game request failed, no organisation ID");
-      return res
-        .status(403)
-        .send({ message: "User not authorized to perform this action." })
-        .end();
-    }
-
-    const userOrganisation = await Organisation.findByPk(
-      req.user.organisationId,
-      {
-        include: [Competition]
-      }
-    );
-
-    if (!userOrganisation) {
-      console.log("Post game request failed, no organisation found");
-
-      return res
-        .status(404)
-        .send({ message: "Linked organisation not found" })
-        .end();
-    }
-
-    const competitionEditableByUser =
-      user.roleId === superAdmin
-        ? true
-        : userOrganisation.competitions.find(
-            competition => competition.dataValues.id === req.body.competitionId
-          );
-    const rolesAllowed = [federation, superAdmin];
-    if (
-      rolesAllowed.includes(userOrganisation.roleId) &&
-      competitionEditableByUser
-    ) {
+    if (req.user.roleId === superAdmin) {
       const newGame = await Game.create(req.body);
       return res.json(newGame);
     } else {
-      console.log(
-        "Post game request failed, user role not in allowed roles or competition not by user's organisation:",
-        userOrganisation.roleId,
-        competitionEditableByUser
+      if (!req.user.organisationId) {
+        console.log("Post game request failed, no organisation ID");
+        return res
+          .status(403)
+          .send({ message: "User not authorized to perform this action." })
+          .end();
+      }
+
+      const userOrganisation = await Organisation.findByPk(
+        req.user.organisationId,
+        {
+          include: [Competition]
+        }
       );
 
-      return res
-        .status(403)
-        .send({ message: "User not authorized to perform this action." })
-        .end();
+      if (!userOrganisation) {
+        console.log("Post game request failed, no organisation found");
+
+        return res
+          .status(404)
+          .send({ message: "Linked organisation not found" })
+          .end();
+      }
+
+      const competitionByUserOrganisation = userOrganisation.competitions.find(
+        competition => competition.dataValues.id === req.body.competitionId
+      );
+      const rolesAllowed = [federation];
+      if (
+        rolesAllowed.includes(userOrganisation.roleId) &&
+        competitionByUserOrganisation
+      ) {
+        const newGame = await Game.create(req.body);
+        return res.json(newGame);
+      } else {
+        console.log(
+          "Post game request failed, user role not in allowed roles or competition not by user's organisation:",
+          userOrganisation.roleId,
+          competitionByUserOrganisation
+        );
+
+        return res
+          .status(403)
+          .send({ message: "User not authorized to perform this action." })
+          .end();
+      }
     }
   } catch (error) {
     next(error);
