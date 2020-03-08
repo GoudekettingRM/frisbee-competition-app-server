7ac09fb7f7513f9b2292f5924b78e8a31e9e4440
GoudekettingRM <r.goudeketting@gmail.com>
2020-03-08 14:19:27 +0100

    fix post endpoint to allow super admins to create teams

--- team/router.js
+++ team/router.js
@@ -3,13 +3,14 @@ const { auth } = require("../auth/authMiddleware");
 const Team = require("./model");
 const User = require("../user/model");
 const Competition = require("../competition/model");
+const { superAdmin } = require("../endpointRoles");
 
 const router = new Router();
 
 router.post("/teams", auth, async (req, res, next) => {
   try {
-    if (!req.user.organisationId) {
-      res
+    if (!req.user.organisationId && req.user.roleId !== superAdmin) {
+      return res
         .status(403)
         .send({ message: "User not authorized to perform this action." })
         .end();
@@ -19,7 +20,7 @@ router.post("/teams", auth, async (req, res, next) => {
       );
 
       if (!currentCompetition) {
-        res
+        return res
           .status(404)
           .send({ message: "Requested competition not found." })
           .end();
@@ -43,7 +44,7 @@ router.get("/teams", async (req, res, next) => {
   try {
     const teams = await Team.findAll();
     if (!teams.length) {
-      res
+      return res
         .status(404)
         .send({ message: "No teams found" })
         .end();
@@ -62,7 +63,7 @@ router.get("/teams/:id", async (req, res, next) => {
       include: [{ model: User, attributes: ["firstName", "lastName"] }]
     });
     if (!team) {
-      res
+      return res
         .status(404)
         .send({ message: "Team not found" })
         .end();
